43e332e1e82f132c91f7a03bc86fb47646c18017
leon-r-h <leo-maus@ro.ru>
2024-10-20 11:33:19 +0200

    Add placeholder standard headers, start with stdarg.h

---
+++ include/stdarg.h
@@ -0,0 +1,13 @@
+#ifndef WIN_C_CRT_ENTRY_H
+#define WIN_C_CRT_ENTRY_H
+
+
+
+#define va_start(ap, arg)	__builtin_va_start((ap), (arg))
+#define va_arg __builtin_va_arg
+#define va_copy __builtin_va_copy
+#define va_end(ap) __builtin_va_end((ap))
+
+
+
+#endif
